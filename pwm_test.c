eecca1ec94fcb3d1aba0c7793ce0fc6e7f2e7370
Javier Cardona <javier.cardona@gmail.com>
2015-11-28 20:17:45 -0800

    Minor improvements to basic test

--- pwm_test.c
+++ pwm_test.c
@@ -1,4 +1,5 @@
 /* Very basic example to test the pwm library
+ * Hook up an LED to pin14 and you should see the intensity change
  */
 #include "espressif/esp_common.h"
 #include "esp/uart.h"
@@ -9,12 +10,16 @@
 void task1(void *pvParameters)
 {
     printf("Hello from task1!\r\n");
-    uint32_t count = 0;
+    // XXX: duty cycle of zero crashes for me.  skip for now
+    uint32_t const init_count = UINT16_MAX/16;
+    uint32_t count = init_count;
     while(1) {
         vTaskDelay(100);
-        pwm_set_duty(count++);
+        printf("duty cycle set to %d/UINT16_MAX%%\r\n", count);
+        pwm_set_duty(count);
+        count += UINT16_MAX/16;
         if (count > UINT16_MAX)
-            count = 0;
+            count = init_count;
     }
 }
 
@@ -28,10 +33,15 @@ void user_init(void)
     printf("pwm_init(1, [14])\n");
     pins[0] = 14;
     pwm_init(1, pins);
+
     printf("pwm_set_freq(1000)     # 1 kHz\n");
     pwm_set_freq(1000);
-    printf("pwm_set_duty(UINT16_MAX/2)    # 50%%\n");
+
+    printf("pwm_set_duty(UINT16_MAX/2)     # 50%%\n");
     pwm_set_duty(UINT16_MAX/2);
+
+    printf("pwm_start()\n");
+    pwm_start();
 
     xTaskCreate(task1, (signed char *)"tsk1", 256, NULL, 2, NULL);
 }
