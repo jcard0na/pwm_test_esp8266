5183c68e52187b8c72cd357eb726aaf7aac4351f
Javier Cardona <javier.cardona@gmail.com>
2015-11-28 18:16:44 -0800

    Simpler pwm test

--- pwm_test.c
+++ pwm_test.c
@@ -6,17 +6,16 @@
 #include "task.h"
 #include "pwm.h"
 
-const int pwm_freq = 46000;
-const int sample_freq = 8000;
-
-static volatile uint16_t frc2_count;
-
-void frc2_interrupt_handler(void)
+void task1(void *pvParameters)
 {
-    /* FRC2 needs the match register updated on each timer interrupt */
-    timer_set_frequency(FRC2, sample_freq);
-    frc2_count++;
-    pwm_set_duty(frc2_count);
+    printf("Hello from task1!\r\n");
+    uint32_t count = 0;
+    while(1) {
+        vTaskDelay(100);
+        pwm_set_duty(count++);
+        if (count > UINT16_MAX)
+            count = 0;
+    }
 }
 
 void user_init(void)
@@ -26,24 +25,13 @@ void user_init(void)
 
     printf("SDK version:%s\n", sdk_system_get_sdk_version());
 
+    printf("pwm_init(1, [14])\n");
     pins[0] = 14;
     pwm_init(1, pins);
-    /* frequency can only be set once */
-    pwm_set_freq(pwm_freq);
+    printf("pwm_set_freq(1000)     # 1 kHz\n");
+    pwm_set_freq(1000);
+    printf("pwm_set_duty(UINT16_MAX/2)    # 50%%\n");
     pwm_set_duty(UINT16_MAX/2);
-    pwm_start();
 
-    /* stop both timers and mask their interrupts as a precaution */
-    timer_set_interrupts(FRC2, false);
-    timer_set_run(FRC2, false);
-
-    /* set up ISRs */
-    _xt_isr_attach(INUM_TIMER_FRC2, frc2_interrupt_handler);
-
-    /* configure timer frequencies */
-    timer_set_frequency(FRC2, sample_freq);
-
-    /* unmask interrupts and start timers */
-    timer_set_interrupts(FRC2, true);
-    timer_set_run(FRC2, true);
+    xTaskCreate(task1, (signed char *)"tsk1", 256, NULL, 2, NULL);
 }
